36d5be3db40161d599fa79f59cd9633ecfb76533
Justin Nearing <justinfnearing@gmail.com>
2015-03-12 06:23:08 -0700

    updating with local latest

--- Assets/Scripts/Character/Inventory.cs
+++ Assets/Scripts/Character/Inventory.cs
@@ -9,53 +9,78 @@ namespace Character
 	//If possible, map the dictionary<name,index> to the objarr name@index
 	//In theory, dictionary allows for fast lookup, object array allows for information storage;
 	public class Inventory : MonoBehaviour {
-		public Dictionary<string,int> inventory;
+		public List<InventoryItem> inventory;
 
 		// Use this for initialization
 		void Start () {
-			inventory = new Dictionary<string,int>();
+			inventory = new List<InventoryItem>();
 		}
 
 		//Add an object/amount to the characters inventory
 		//increases amount if key already exists
-		public void Add(string obj, int amount)
+		public void Add(string name, int amount, object obj)
 		{
-
-			if (inventory.ContainsKey(obj))
+			for (int i=0; i<inventory.Count; i++)
 			{
-				amount += inventory [obj];
-				inventory.Remove(obj);
+				if (inventory [i].name == name)
+				{
+					inventory[i].amount+=amount;
+					return;
+				}
 			}
-			Debug.Log(obj + ", " + amount);
-			inventory.Add(obj,amount);
-			Debug.Log(inventory [obj]);
+			Debug.Log(name + ", " + amount + ", " + obj);
+			inventory.Add(new InventoryItem(name, amount, obj));
+		}
+
+		//Add an object name/amount to the characters inventory
+		//Allow for inventory name without an object reference
+		public void Add(string name, int amount)
+		{
+			for (int i=0; i<inventory.Count; i++)
+			{
+				if (inventory [i].name == name)
+				{
+					inventory[i].amount+=amount;
+					return;
+				}
+			}
+			Debug.Log(name + ", " + amount);
+			inventory.Add(new InventoryItem(name, amount));
 		}
 
 		//Get the amount of an object the character has in their inventory
-		public int GetObjectAmount(string obj)
+		public int GetObjectAmount(string name)
 		{
-			if (inventory.ContainsKey(obj))
+			for (int i =0; i<inventory.Count; i++)
 			{
-				return inventory [obj];
-			} else
-			{
-				return 0;
+				if (inventory[i].name == name)
+				{
+					return inventory[i].amount;
+				}
 			}
+			return 0;
 		}
 
 		//Remove an amount of an object from the characters inventory
 		//Returns -1 if object does not exist or does not have enough of the amount
 		//Will not remove the amount if the amount is less than zero
-		public int RemoveAmountFromObject(string obj, int amount)
+		public int RemoveAmountFromInventory(string name, int amount)
 		{
-			int newAmount = inventory [obj] - amount;
+			int i;
+			for (i=0;i<inventory.Count;i++)
+			{
+				if (inventory[i].name == name)
+				{
+					break; //we now know the index of the matching item; there should never be duplicates
+				}
+			}
+			int newAmount = inventory [i].amount - amount;
 			if (newAmount > 0)
 			{
-				inventory.Remove(obj);
-				inventory.Add(obj, newAmount);
+				inventory[i].amount = newAmount;
 			} else if (newAmount == 0)
 			{
-				inventory.Remove(obj);
+				inventory.RemoveAt(i);
 			} else if (newAmount < 0)
 			{
 				return -1;
@@ -63,17 +88,35 @@ namespace Character
 			return newAmount;
 		}
 
+		//check if the inv name is in the inventory, return bool
 		public bool HasObjectNameInInventory(string toFind)		
 		{
 			Debug.Log("GetFoodFromInventory");
 			if (GetObjectAmount(toFind) == 0)
 			{
 				return false;
-			} else
-			{
-				RemoveAmountFromObject(toFind,1);
-				return true;
-			}
+			} 
+			return true;
+		}
+	}
+
+	//inventory item, allows for a list of item variables to be stored.
+	public class InventoryItem : Object
+	{
+		public int amount;
+		public object obj;
+
+		public InventoryItem(string name, int amount, object obj)
+		{
+			this.name = name;
+			this.amount = amount;
+			this.obj = obj;
+		}
+
+		public InventoryItem(string name, int amount)
+		{
+			this.name = name;
+			this.amount = amount;
 		}
 	}
 }
