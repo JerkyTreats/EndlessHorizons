17df90d4f79a3fafa820e8a2592ddd57747ca890
Justin Nearing <justinfnearing@gmail.com>
2015-03-13 06:57:25 -0700

    characterRefactor remaining files

---
+++ Assets/Scripts/Character/NPC/Goal.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+
+namespace NPC
+{
+    //goal object to create, makes for easier (conceptually) goalWeight sorting;
+    public class Goal : ScriptableObject
+    {
+        public string goalName;
+        public int goalWeight;
+
+        public void Init(string name, int weight)
+        {
+            goalName = name;
+            goalWeight = weight;
+        }
+    }
+}
