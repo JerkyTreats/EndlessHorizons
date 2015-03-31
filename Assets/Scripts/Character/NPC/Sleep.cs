6f547818b061ab85d63d0a50d7aedace6d12c9c9
Justin Nearing <justinfnearing@gmail.com>
2015-03-31 16:37:41 -0700

    started Sleep functionality. Decided I will need a base class to derive Sleep/Hunger/etc.

---
+++ Assets/Scripts/Character/NPC/Sleep.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace NPC
+{
+	public class Sleep : Character.Sleep {
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
+}
