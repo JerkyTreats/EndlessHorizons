90b43406cbe077c4fd47a78a57b413267671f512
Justin Nearing <justinfnearing@gmail.com>
2015-03-31 14:46:54 -0700

    too many cooks fix code cleanup

--- Assets/Scripts/Kitchen/KitchenTrigger.cs
+++ Assets/Scripts/Kitchen/KitchenTrigger.cs
@@ -5,27 +5,17 @@ using System.Collections.Generic;
 namespace Kitchen
 {
 	public class KitchenTrigger : MonoBehaviour {
-//		public Character.Character characterInTrigger;
-//		public bool isActive;
-//		private int numberOfCooks = 0;
 		public List<Character.Character> charactersInTrigger;
 
 		void Start()
 		{
 			charactersInTrigger = new List<Character.Character>();
-//			characterInTrigger = null;
-//			isActive = false;
 		}
 
 		void OnTriggerEnter2D(Collider2D other)
 		{
 			charactersInTrigger.Add (other.gameObject.GetComponent<Character.Character>());
-			//isActive = true;
 			Debug.Log(other.gameObject.name + " has entered the kitchen trigger");
-//			if (characterInTrigger == null)
-//			{ 
-//				characterInTrigger = other.gameObject.GetComponent<Character.Character>();
-//			}
 		}
 
 		void OnTriggerExit2D(Collider2D other)
@@ -38,16 +28,6 @@ namespace Kitchen
 			{
 				Debug.LogError ("Character " + other + " was not in charactersInTriggerList");
 			}
-
-//			numberOfCooks = - 1;
-//			if (numberOfCooks==0)
-//			{
-//				isActive = false;
-//				characterInTrigger = null;
-//			} else {
-//				characterInTrigger = other.gameObject.GetComponent<Character.Character>();
-//			}
-//			Debug.Log(other.gameObject.name + " has entered the kitchen trigger");
 		}
 	}
 }
