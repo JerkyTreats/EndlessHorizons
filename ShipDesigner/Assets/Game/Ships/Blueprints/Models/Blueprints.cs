6b8984c7b4da2c3b15aa04131c53f281113ff8c6
Justin Nearing <justinfnearing@gmail.com>
2017-05-01 17:10:40 -0700

    Add doc comments for Blueprints

--- ShipDesigner/Assets/Game/Ships/Blueprints/Models/Blueprints.cs
+++ ShipDesigner/Assets/Game/Ships/Blueprints/Models/Blueprints.cs
@@ -51,6 +51,10 @@ namespace Ships.Blueprints
 			ContainerMap.Add(key, container);
 		}
 
+		/// <summary>
+		/// Get a Filename for the blueprint. Intended to be used for filename when saved to disk
+		/// </summary>
+		/// <returns></returns>
 		public string GetFileName()
 		{
 			if (string.IsNullOrEmpty(m_fileName))
@@ -58,6 +62,9 @@ namespace Ships.Blueprints
 			return m_fileName;
 		}
 
+		/// <summary>
+		/// Updates filename to be used when the blueprint is saved to disk
+		/// </summary>
 		public void SetFileName()
 		{
 			string prefix = string.IsNullOrEmpty(Name) ? Name + "_" : "Blueprint_";
@@ -66,7 +73,10 @@ namespace Ships.Blueprints
 			m_fileName = newName;
 		}
 
-
+		/// <summary>
+		/// Returns the blueprint object to be saved to disk.
+		/// </summary>
+		/// <returns>BlueprintSaveObject containing only the data that will be saved to disk</returns>
 		public BlueprintSaveObject GetSaveObject()
 		{
 			return new BlueprintSaveObject(Name, Containers);
