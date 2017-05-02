4dea5ca33658c859d0ae0e21e849076dfaa98791
Justin Nearing <justinfnearing@gmail.com>
2017-05-01 17:03:40 -0700

    Add doc comments to, and update filename of, BlueprintComponentContainer

--- ShipDesigner/Assets/Game/Ships/Blueprints/BlueprintComponents.cs
+++ ShipDesigner/Assets/Game/Ships/Blueprints/BlueprintComponents.cs
@@ -4,12 +4,19 @@ using System.Collections.Generic;
 
 namespace Ships.Blueprints
 {
+	/// <summary>
+	/// An object containing all BlueprintComponents of a single type
+	/// </summary>
 	public class BlueprintComponentContainer
 	{
 		[JsonConverter(typeof(StringEnumConverter))]
 		public Blueprints.ComponentKey Key { get; set; }
 		public List<BlueprintComponent> Components { get; set; }
 
+		/// <summary>
+		/// Constructs a new BlueprintComponent Container
+		/// </summary>
+		/// <param name="key">The type of Components that wil be contained</param>
 		public BlueprintComponentContainer(Blueprints.ComponentKey key)
 		{
 			Key = key;
