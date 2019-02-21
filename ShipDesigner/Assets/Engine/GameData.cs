b1ee115c42a9570fb30dc2ec1fffb10bb597da1a
JerkyTreats <justinfnearing@gmail.com>
2019-02-20 20:40:33 -0800

    Remove ambiguous Grid reference

--- ShipDesigner/Assets/Engine/GameData.cs
+++ ShipDesigner/Assets/Engine/GameData.cs
@@ -68,7 +68,7 @@ namespace Engine
 		public void SetGrid(GameObject grid)
 		{
 			Grid = grid;
-			m_zAxisItemPlacement = Grid.GetComponent<Grid>().ZAxisItemPlacement;
+			m_zAxisItemPlacement = Grid.GetComponent<Workshop.Grid.Grid>().ZAxisItemPlacement;
 		}
 
 		/// <summary>
